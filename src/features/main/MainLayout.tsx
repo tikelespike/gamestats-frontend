9b4d21088aef997e654b75bb9e2d1b65b7f1c953
Timo Weberruß <timo.weberruss@student.kit.edu>
2025-01-22 23:44:14 +0100

    Add navbar placeholder

--- src/features/main/MainLayout.tsx
+++ src/features/main/MainLayout.tsx
@@ -1,33 +1,29 @@
 import React from "react"
 import { useDisclosure } from "@mantine/hooks"
-import { AppShell, Burger, Group, Skeleton } from "@mantine/core"
+import { AppShell, Burger, Group, Text } from "@mantine/core"
+import { NavBar } from "./NavBar"
 
 const MainLayout = () => {
-  const [opened, { toggle }] = useDisclosure();
-
+  const [opened, { toggle }] = useDisclosure()
 
   return (
     <AppShell
       header={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
       padding="md"
     >
       <AppShell.Header>
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Text size="xl">Blood Manager</Text>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
+        <NavBar />
       </AppShell.Navbar>
       <AppShell.Main>Main</AppShell.Main>
     </AppShell>
-  );
+  )
 }
 
 export default MainLayout
---
+++ src/features/main/NavBar.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import {
+  Icon2fa,
+  IconBellRinging,
+  IconDatabaseImport,
+  IconFingerprint,
+  IconKey,
+  IconLogout,
+  IconReceipt2,
+  IconSettings,
+  IconSwitchHorizontal,
+} from "@tabler/icons-react"
+import classes from "./NavBar.module.css"
+
+const data = [
+  { link: "", label: "Notifications", icon: IconBellRinging },
+  { link: "", label: "Billing", icon: IconReceipt2 },
+  { link: "", label: "Security", icon: IconFingerprint },
+  { link: "", label: "SSH Keys", icon: IconKey },
+  { link: "", label: "Databases", icon: IconDatabaseImport },
+  { link: "", label: "Authentication", icon: Icon2fa },
+  { link: "", label: "Other Settings", icon: IconSettings },
+]
+
+export function NavBar() {
+  const [active, setActive] = useState("Billing")
+
+  const links = data.map(item => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={event => {
+        event.preventDefault()
+        setActive(item.label)
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ))
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>{links}</div>
+
+      <div className={classes.footer}>
+        <a
+          href="#"
+          className={classes.link}
+          onClick={event => event.preventDefault()}
+        >
+          <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
+          <span>Change account</span>
+        </a>
+
+        <a
+          href="#"
+          className={classes.link}
+          onClick={event => event.preventDefault()}
+        >
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  )
+}
