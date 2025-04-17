a92fd40fe6f64a0903c87fbf275cafa61e48a36e
Timo Weberruß <timo.weberruss@student.kit.edu>
2025-04-17 11:25:26 +0200

    Add more placeholder data

--- src/features/games/GameManager.tsx
+++ src/features/games/GameManager.tsx
@@ -21,6 +21,48 @@ const GameManager = () => {
       imageUrl:
         "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/demon/Imp_icon.webp",
     },
+    {
+      id: 4,
+      name: "John",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Fortune%20Teller_icon.webp",
+    },
+    {
+      id: 5,
+      name: "Sarah",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/minion/Baron_icon.webp",
+    },
+    {
+      id: 6,
+      name: "Michael",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Undertaker_icon.webp",
+    },
+    {
+      id: 7,
+      name: "Alice",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Investigator_icon.webp",
+    },
+    {
+      id: 8,
+      name: "Bob",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Washerwoman_icon.webp",
+    },
+    {
+      id: 9,
+      name: "Charlie",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Slayer_icon.webp",
+    },
+    {
+      id: 10,
+      name: "David",
+      imageUrl:
+        "https://script.bloodontheclocktower.com/images/icon/1%20-%20Trouble%20Brewing/townsfolk/Virgin_icon.webp",
+    },
   ]
 
   return (
