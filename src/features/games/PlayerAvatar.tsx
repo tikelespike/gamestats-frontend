0926d35bc273a856e6c9fa2be23a532ed9e11ece
Timo Weberruß <timo.weberruss@student.kit.edu>
2025-04-19 23:55:58 +0200

    Fix avatar text override

--- src/features/games/PlayerAvatar.tsx
+++ src/features/games/PlayerAvatar.tsx
@@ -66,13 +66,14 @@ const PlayerAvatar = ({
         <Skeleton width={"80px"} height={"15px"} />
       ) : (
         <Text size="sm" fw={500} ta="center">
-          {overrideText || participation?.playerId ? (
-            players.data
-              ?.find(p => p.id === participation?.playerId)
-              ?.name.split(" ")[0] + (isWinner ? " 🏆" : "")
-          ) : (
-            <i>Unknown Player</i>
-          )}
+          {overrideText ||
+            (participation?.playerId ? (
+              players.data
+                ?.find(p => p.id === participation?.playerId)
+                ?.name.split(" ")[0] + (isWinner ? " 🏆" : "")
+            ) : (
+              <i>Unknown Player</i>
+            ))}
         </Text>
       )}
     </Stack>
